105837b1da88fe28e29d1539b14d606919e81d5f
lijoshabraham <lijoshabraham96@gmail.com>
2023-11-18 02:20:07 -0500

    Create Scanner object to read input from the user and called the class

--- src/amusementpark/AmusementParkForLoopMain.java
+++ src/amusementpark/AmusementParkForLoopMain.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class AmusementParkForLoopMain {
 
 	public static void main(String[] args) {
+		// Create object
+		AmusementParkForLoop amusementParkForLoop = new AmusementParkForLoop();
 
+		// Create Scanner object to read input from the user
+		Scanner sc = new Scanner(System.in);
+
+		// Check height eligibility
+		amusementParkForLoop.checkHeightEligibility();
 	}
 
 }
