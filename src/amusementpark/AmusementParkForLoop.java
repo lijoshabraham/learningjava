931f64915c2a43cb033a3772c343f0b43c9581cb
lijoshabraham <lijoshabraham96@gmail.com>
2023-11-18 02:18:26 -0500

    wrote code to allow only 10 people between 91-199cm

--- src/amusementpark/AmusementParkForLoop.java
+++ src/amusementpark/AmusementParkForLoop.java
@@ -3,5 +3,44 @@ package amusementpark;
 import java.util.Scanner;
 
 public class AmusementParkForLoop {
-	
+	// Declare a variable for height
+	int height;
+
+	// Counter to keep track of the number of people
+	int numberOfPeople = 0;
+
+	// create a method to check height eligibility
+	void checkHeightEligibility() {
+
+		for (numberOfPeople = 0; numberOfPeople < 10;) {
+			
+			 // Create Scanner object to read input from the user
+			Scanner sc = new Scanner(System.in);
+			
+			// Receive input from the user for height
+			System.out.println("Enter height for person in cm, or enter 1 to exit:");
+			height = sc.nextInt();
+
+			// Check if the user wants to exit
+			if (height == 1) {
+				System.out.println("Exiting.");
+				sc.close();
+				break;
+			}
+
+			// check if the person is eligible for the ride
+			if (height > 90 && height < 200) {
+				System.out.println("Allowed to ride!");
+				// Increment the number of people
+				numberOfPeople++;
+			} else {
+				System.out.println("Not allowed to ride.");
+			}
+		}
+		
+		// Check if the maximum number of riders is reached
+		if (numberOfPeople == 10) {
+			System.out.println("Maximum riders reached");
+		}
+	}
 }
