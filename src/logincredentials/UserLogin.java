82cc75dd63fb59e80d1cedecaeb5905403e6c939
lijoshabraham <lijoshabraham96@gmail.com>
2023-11-19 14:07:08 -0500

    added 3 attempts for user to enter correct credentials

--- src/logincredentials/UserLogin.java
+++ src/logincredentials/UserLogin.java
@@ -3,27 +3,31 @@ package logincredentials;
 import java.util.Scanner;
 
 public class UserLogin {
-	
-	String userId =  "pivotAdmin";
+
+	String userId = "pivotAdmin";
 	String password = "Admin123";
-	
+	int MaxAttempts = 3;
+
 	// create a method to check user login credentials
 	void loginCheck() {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter Username: ");
-		String InputUserId = sc.next();
-		
-		System.out.println("Enter Password: ");
-		String InputPassword = sc.next();
 
-		if((InputUserId.equals(userId)) && (InputPassword.equals(password))) {
-			System.out.println("You are logged in to the application");
+		Scanner sc = new Scanner(System.in);
+
+
+		for (int attempts = 0; attempts < MaxAttempts; attempts++) {
+			System.out.println("Enter Username: ");
+			String InputUserId = sc.next();
+
+			System.out.println("Enter Password: ");
+			String InputPassword = sc.next();
+			if ((InputUserId.equals(userId)) && (InputPassword.equals(password))) {
+				System.out.println("You are logged in to the application");
+				return;
+			} else {
+				System.out.println("Incorrect User id or password.Try again");
+			}
 		}
-		else {
-			System.out.println("Incorrect User id or password.Try again");
-		}
-		
+		System.out.println("Account locked");
+
 	}
 }
