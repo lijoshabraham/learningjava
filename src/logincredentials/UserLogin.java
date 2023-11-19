9c3fc1ae7464d16d51eb55d2a8a38b187b95ed5c
lijoshabraham <lijoshabraham96@gmail.com>
2023-11-19 12:54:50 -0500

    created class for user login check program

---
+++ src/logincredentials/UserLogin.java
@@ -0,0 +1,5 @@
+package logincredentials;
+
+public class UserLogin {
+
+}
---
+++ src/logincredentials/UserLoginMain.java
@@ -0,0 +1,10 @@
+package logincredentials;
+
+public class UserLoginMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
