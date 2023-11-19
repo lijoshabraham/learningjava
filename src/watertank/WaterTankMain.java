1e245322e25220c69f9ea26dbfe0d3625469272f
lijoshabraham <lijoshabraham96@gmail.com>
2023-11-19 12:33:08 -0500

    created object in the main class

--- src/watertank/WaterTankMain.java
+++ src/watertank/WaterTankMain.java
@@ -2,9 +2,11 @@ package watertank;
 
 public class WaterTankMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // Create a water tank object
+        WaterTank waterTank = new WaterTank();
 
-	}
-
+        // Fill the water tank
+        waterTank.fillWaterTank();
+    }
 }
