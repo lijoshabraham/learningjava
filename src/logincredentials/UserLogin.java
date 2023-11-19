31bdfd312e2d0781685a972947fe9d31110dc6dd
lijoshabraham <lijoshabraham96@gmail.com>
2023-11-19 13:25:10 -0500

    created class to check user credentials

--- src/logincredentials/UserLogin.java
+++ src/logincredentials/UserLogin.java
@@ -1,5 +1,29 @@
 package logincredentials;
 
-public class UserLogin {
+import java.util.Scanner;
 
+public class UserLogin {
+	
+	String userId =  "pivotAdmin";
+	String password = "Admin123";
+	
+	// create a method to check user login credentials
+	void loginCheck() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Username: ");
+		String InputUserId = sc.next();
+		
+		System.out.println("Enter Password: ");
+		String InputPassword = sc.next();
+
+		if((InputUserId.equals(userId)) && (InputPassword.equals(password))) {
+			System.out.println("You are logged in to the application");
+		}
+		else {
+			System.out.println("Incorrect User id or password.Try again");
+		}
+		
+	}
 }
