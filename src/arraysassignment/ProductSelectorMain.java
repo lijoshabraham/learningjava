6eb9f14153282460e557c6360e4e46352004ef2d
lijoshabraham <lijoshabraham96@gmail.com>
2023-11-25 18:21:14 -0500

    created object

--- src/arraysassignment/ProductSelectorMain.java
+++ src/arraysassignment/ProductSelectorMain.java
@@ -4,6 +4,8 @@ public class ProductSelectorMain {
 
 	public static void main(String[] args) {
 		ProductSelector productSelector = new ProductSelector();
+		
+		productSelector.displayProducts();
 
 	}
 
