a51b018f4f8730a15ad7892a756e05efde1d266f
lijoshabraham <lijoshabraham96@gmail.com>
2023-11-25 14:15:14 -0500

    created object and and called the method

--- src/arraysassignment/SumPairMain.java
+++ src/arraysassignment/SumPairMain.java
@@ -4,6 +4,9 @@ public class SumPairMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		SumPair sumpair = new SumPair();
+		
+		sumpair.displaySumPair();
 
 	}
 
