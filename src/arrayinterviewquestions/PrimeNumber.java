70ea92d827e60b98b69d4fca11039ee2478f6d1d
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-01 01:05:50 -0500

    added logic to find prime number

--- src/arrayinterviewquestions/PrimeNumber.java
+++ src/arrayinterviewquestions/PrimeNumber.java
@@ -1,5 +1,40 @@
 package arrayinterviewquestions;
 
-public class PrimeNumber {
+import java.util.Scanner;
 
+public class PrimeNumber {
+	Scanner sc = new Scanner(System.in);
+	double inputNumber;
+
+	double getNumberFromUser() {
+		System.out.println("Enter a positive number");
+		inputNumber = sc.nextDouble();
+
+		while (inputNumber <= 0) {
+			System.out.println("Please enter a positive number:");
+			inputNumber = sc.nextDouble();
+		}
+		return inputNumber;
+	}
+	
+	boolean isPrime() {
+		for (int i = 2; i <= Math.sqrt(inputNumber); i++) {
+			if (inputNumber % i == 0) {
+				return false;
+			}
+		}
+		if (inputNumber <= 1) {
+			return false;
+		}
+		
+		return true;
+	}
+
+	void displayResult() {
+		if (isPrime()) {
+			System.out.println(inputNumber + " is a prime number");
+		} else {
+			System.out.println(inputNumber + " is not a prime number");
+		}
+	}
 }
