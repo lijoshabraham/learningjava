af1366fe8f00434f5485dd9a22f2f0f6cb7d324b
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-01 01:07:32 -0500

    created object and method call

--- src/arrayinterviewquestions/PrimeNumberMain.java
+++ src/arrayinterviewquestions/PrimeNumberMain.java
@@ -5,6 +5,11 @@ public class PrimeNumberMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		PrimeNumber primeNumber = new PrimeNumber();
 
+		primeNumber.getNumberFromUser();
+		
+		primeNumber.displayResult();
+
+	}
 }
