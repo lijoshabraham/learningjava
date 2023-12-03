c656b864283ff6caa57cff3de54c6d8c82cbc905
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-03 12:44:05 -0500

    created object and called method

---
+++ src/arrayinterviewquestions/FindLargestNumberMain.java
@@ -0,0 +1,14 @@
+package arrayinterviewquestions;
+
+public class FindLargestNumberMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FindLargestNumber findLargestNumber = new FindLargestNumber();
+		
+		findLargestNumber.userInputs();
+
+	}
+
+}
