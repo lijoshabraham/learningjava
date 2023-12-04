0910ac4dba4ea63ae2ea9c9f9ac20ea5f38751eb
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-04 14:17:24 -0500

    Taking user input, created instance and method call

---
+++ src/arrayinterviewquestions/SecondHighestNumberMain.java
@@ -0,0 +1,29 @@
+package arrayinterviewquestions;
+
+import java.util.Scanner;
+
+public class SecondHighestNumberMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the array length");
+		int arrayLength = sc.nextInt();
+
+		System.out.println("Enter array elements");
+		int[] arrayElements = new int[arrayLength];
+		for (int i = 0; i < arrayLength; i++) {
+			arrayElements[i] = sc.nextInt();
+		}
+
+		SecondHighestNumber secondHighestNumber = new SecondHighestNumber();
+
+		secondHighestNumber.arrayElements = arrayElements;
+
+		secondHighestNumber.arraySorting();
+
+		sc.close();
+	}
+
+}
