048f4d3a57e604bd4d895ce5c60c682d71a9f075
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-04 15:21:43 -0500

    created object and method calls

---
+++ src/arrayinterviewquestions/ReverseArrayInPlaceMain.java
@@ -0,0 +1,16 @@
+package arrayinterviewquestions;
+
+public class ReverseArrayInPlaceMain {
+
+    public static void main(String[] args) {
+        ReverseArrayInPlace reverseArrayInPlace = new ReverseArrayInPlace();
+
+        reverseArrayInPlace.takeUserInput();
+
+        reverseArrayInPlace.printOriginalArray();
+
+        reverseArrayInPlace.reverseArrayInPlace();
+
+        reverseArrayInPlace.printReversedArray();
+    }
+}
