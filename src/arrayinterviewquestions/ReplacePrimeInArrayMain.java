d19ca778f800f787677c4b3a4b32fba4304252dc
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-04 16:40:33 -0500

    created object and method calls

---
+++ src/arrayinterviewquestions/ReplacePrimeInArrayMain.java
@@ -0,0 +1,18 @@
+package arrayinterviewquestions;
+
+public class ReplacePrimeInArrayMain {
+
+	public static void main(String[] args) {
+		ReplacePrimeInArray replacePrimeInArray = new ReplacePrimeInArray();
+
+		replacePrimeInArray.takeUserInput();
+
+		replacePrimeInArray.printOriginalArray();
+
+		replacePrimeInArray.replacePrimeInArray();
+
+		replacePrimeInArray.printReplacedArray();
+
+	}
+
+}
