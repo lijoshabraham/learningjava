e887f39dc1841a6aeb4f580c774936f973019ab2
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-04 14:57:16 -0500

    created instance and method call

---
+++ src/arrayinterviewquestions/FindDuplicateNumbersMain.java
@@ -0,0 +1,12 @@
+package arrayinterviewquestions;
+
+public class FindDuplicateNumbersMain {
+
+	public static void main(String[] args) {
+
+		FindDuplicateNumbers findDuplicateNumbers = new FindDuplicateNumbers();
+
+		findDuplicateNumbers.userInput();
+	}
+
+}
