e98c8681d1f38674ffdc195917731ec6571ef507
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-04 14:56:49 -0500

    added code to find and print duplicate numbers in an array

---
+++ src/arrayinterviewquestions/FindDuplicateNumbers.java
@@ -0,0 +1,33 @@
+package arrayinterviewquestions;
+
+import java.util.Scanner;
+
+public class FindDuplicateNumbers {
+	int[] arrayElements;
+
+	void userInput() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the array length");
+		int arrayLength = sc.nextInt();
+
+		System.out.println("Enter array elements");
+		arrayElements = new int[arrayLength];
+		for (int i = 0; i < arrayLength; i++) {
+			arrayElements[i] = sc.nextInt();
+		}
+		findAndPrintDuplicates();
+		sc.close();
+	}
+
+	void findAndPrintDuplicates() {
+		System.out.println("\nRepeated numbers in the array:");
+		for (int i = 0; i < arrayElements.length - 1; i++) {
+			for (int j = i + 1; j < arrayElements.length; j++) {
+				if (arrayElements[i] == arrayElements[j]) {
+					System.out.println(arrayElements[i]);
+				}
+			}
+		}
+	}
+}
