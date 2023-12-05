13c69eec6cccf2f3c529f323148a97f3fca2c5c9
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-05 02:10:24 -0500

    receiving user input and changed logic to find descending order

--- src/arrayinterviewquestions/DescendingOrderArray.java
+++ src/arrayinterviewquestions/DescendingOrderArray.java
@@ -1,28 +1,49 @@
 package arrayinterviewquestions;
 
-public class DescendingOrderArray {
-    int[] arrayElements;
+import java.util.Scanner;
 
-    void printArrayElements() {
-        System.out.print("\nArray Elements: [");
-        for (int i = 0; i < arrayElements.length; i++) {
-            System.out.print(arrayElements[i]);
-            if (i < arrayElements.length - 1) {
+public class DescendingOrderArray {
+
+	Scanner sc = new Scanner(System.in);
+	int[] arrayElements;
+	int arrayLength;
+
+	void userInput() {
+		System.out.println("Enter the array length");
+		arrayLength = sc.nextInt();
+
+		System.out.println("Enter array elements");
+		arrayElements = new int[arrayLength];
+		
+		for (int i = 0; i < arrayLength; i++) {
+			arrayElements[i] = sc.nextInt();
+		}
+
+		sc.close();
+		printArrayElements();
+	}
+
+	void printArrayElements() {
+		System.out.print("\nOriginal array Elements: [");
+		for (int i = 0; i < arrayElements.length; i++) {
+			System.out.print(arrayElements[i]);
+			if (i < arrayElements.length - 1) {
 				System.out.print(", ");
 			}
-        }
-        System.out.println("]");
-    }
+		}
+		System.out.println("]");
+		printDescendingOrder();
+	}
 
-    void printDescendingOrder() {
-        System.out.print("\nArray in Descending Order: [");
-        for (int i = arrayElements.length - 1; i >= 0; i--) {
-            System.out.print(arrayElements[i]);
-            if (i > 0) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println("]");
-    }
+	void printDescendingOrder() {
+		System.out.print("\nArray in Descending Order: [");
+		for (int i = arrayLength - 1; i >= 0; i--) {
+			System.out.print(arrayElements[i]);
+			if (i > 0) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("]");
+	}
 
 }
