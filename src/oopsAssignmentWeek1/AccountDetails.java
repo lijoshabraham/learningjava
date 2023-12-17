4593eb364c61cede09812693c3e89178ee0b1485
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-17 17:47:17 -0500

    person account details

--- src/oopsAssignmentWeek1/AccountDetails.java
+++ src/oopsAssignmentWeek1/AccountDetails.java
@@ -2,4 +2,40 @@ package oopsAssignmentWeek1;
 
 public class AccountDetails {
 
+	public String accountNumber;
+	public String nameOfAccHolder;
+	private double availableBalance;
+	private int pin;
+	private String onlineBankPass;
+	
+	public AccountDetails(String accountNumber, String nameOfAccHolder, double availableBalance, int pin,
+			String onlineBankPass) {
+		super();
+		this.accountNumber = accountNumber;
+		this.nameOfAccHolder = nameOfAccHolder;
+		this.availableBalance = availableBalance;
+		this.pin = pin;
+		this.onlineBankPass = onlineBankPass;
+	}
+
+	public int getPin() {
+		return pin;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public double getAvailableBalance() {
+		return availableBalance;
+	}
+
+	public String getOnlineBankPass() {
+		return onlineBankPass;
+	}
+
+	public void setAvailableBalance(double newBalance) {
+		this.availableBalance = newBalance;
+	}
+
 }
