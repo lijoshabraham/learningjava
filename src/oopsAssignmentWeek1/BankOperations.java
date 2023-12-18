5a1d7dcbd61f46a725298718fee219907a6ec81b
lijoshabraham <lijoshabraham96@gmail.com>
2023-12-18 15:59:28 -0500

    created methods

--- src/oopsAssignmentWeek1/BankOperations.java
+++ src/oopsAssignmentWeek1/BankOperations.java
@@ -1,5 +1,32 @@
 package oopsAssignmentWeek1;
 
 public class BankOperations {
+	private double balance;
+
+	public BankOperations(double balance) {
+		this.balance = balance;
+	}
+
+	public void withdraw(double amount) {
+		if (amount > 0 && amount <= balance) {
+			balance -= amount;
+			System.out.println("Withdrawal successful! Remaining balance: $" + balance);
+		} else {
+			System.out.println("Invalid withdrawal amount or insufficient funds.");
+		}
+	}
+
+	public void deposit(double amount) {
+		if (amount > 0) {
+			balance += amount;
+			System.out.println("Deposit successful! New balance: $" + balance);
+		} else {
+			System.out.println("Invalid deposit amount.");
+		}
+	}
+
+	public void viewBalance() {
+		System.out.println("Current balance: $" + balance);
+	}
 
 }
