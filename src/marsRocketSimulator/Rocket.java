26276e9850d3361324ad3fc9178b4efd35d49d88
lijoshabraham <lijoshabraham96@gmail.com>
2024-01-13 14:11:04 -0500

    Created class Rocket that implements SpaceShip class

---
+++ src/marsRocketSimulator/Rocket.java
@@ -0,0 +1,29 @@
+package marsRocketSimulator;
+
+public class Rocket implements SpaceShip {
+
+	@Override
+	public boolean launch() {
+
+		return false;
+	}
+
+	@Override
+	public boolean land() {
+
+		return false;
+	}
+
+	@Override
+	public boolean canCarry(Item item) {
+
+		return false;
+	}
+
+	@Override
+	public boolean carry(Item item) {
+
+		return false;
+	}
+
+}
