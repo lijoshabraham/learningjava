8d0cf632a9ade1d7f0e8c062b4186f4b1db2e77d
lijoshabraham <lijoshabraham96@gmail.com>
2024-01-15 03:37:34 -0500

    added probability equation for launch and land method

--- src/marsRocketSimulator/R1.java
+++ src/marsRocketSimulator/R1.java
@@ -8,22 +8,36 @@ public class R1 extends Rocket {
 		setRocketMaximumWeight(18000); // Maximum rocket weight with cargo in kilograms
 		setLaunchExplosionRate(0.05); // rate of launch explosion is 5%
 		setLandingExplosionRate(0.01); // rate of landing explosion is 1%
-		setCargoLimit(getRocketMaximumWeight()-getRocketCost());
+		setCargoLimit(getRocketMaximumWeight() - getRocketCost());
 		setCurrentWeight(getRocketWeight());
 	}
 
 	@Override
-	//overriding the launch method to calculate the corresponding chance of exploding
+	// launch method to calculate the corresponding chance of explosion
+	/*
+	 * If the Chance of launch explosion is greater than random number generated
+	 * then it failed else it launches success
+	 */
 	public boolean launch() {
-		setChanceOfLaunchExplosion(getLaunchExplosionRate()*(getCargoCarried()/getCargoLimit()));
-		return launch();
+		setChanceOfLaunchExplosion(getLaunchExplosionRate() * (getCargoCarried() / getCargoLimit()));
+		if ((getChanceOfLaunchExplosion()) > randomNumber()) {
+			// launch failed
+			return launch();
+		}
+		// launch successful
+		return true;
 	}
 
 	@Override
-	//override the land method to calculate the corresponding chance of crash
+	// override the land method to calculate the corresponding chance of crash
 	public boolean land() {
-		setChanceOfLandingCrash(getLandingExplosionRate()*(getCargoCarried()/getCargoLimit()));
-		return land();
+		setChanceOfLandingCrash(getLandingExplosionRate() * (getCargoCarried() / getCargoLimit()));
+		if ((getChanceOfLaunchExplosion()) > randomNumber()) {
+			// launch failed
+			return land();
+		}
+		// launch successful
+		return true;
 	}
 
 }
