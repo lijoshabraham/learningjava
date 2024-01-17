a42e6dc050f1aab63c6acbd9f4315a3107ec7f26
lijoshabraham <lijoshabraham96@gmail.com>
2024-01-16 20:46:02 -0500

    added code to run the program

--- src/marsRocketSimulator/Main.java
+++ src/marsRocketSimulator/Main.java
@@ -1,11 +1,21 @@
 package marsRocketSimulator;
 
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Main {
-	public static void main(String[] args) throws FileNotFoundException {
-		Simulation simulation = new Simulation();
-		simulation.loadItems();
-	}
 
+	public static void main(String[] args) {
+		Simulation simulation = new Simulation();
+
+		ArrayList<Item> items = simulation.loadItems();
+		ArrayList<Rocket> r1Rockets = simulation.loadU1(items);
+		ArrayList<Rocket> r2Rockets = simulation.loadU2(items);
+
+		System.out
+				.println("Total budget required for R1 rockets: $" + simulation.runSimulation(r1Rockets) + " Millions");
+
+		System.out
+				.println("Total budget required for R2 rockets: $" + simulation.runSimulation(r2Rockets) + " Millions");
+
+	}
 }
